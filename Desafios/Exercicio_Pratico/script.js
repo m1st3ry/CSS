03f7b66641b936022858ac82770b4a1e3824f33d
Robinson Vatam <robinson.vatam@protonmail.com>
2022-06-30 23:39:55 -0300

    adicionando nossas linhas JS

---
+++ Desafios/Exercicio_Pratico/script.js
@@ -0,0 +1,7 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const btnMobile = document.getElementById('btn-mobile');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
